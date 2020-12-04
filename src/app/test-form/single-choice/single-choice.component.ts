6bfbb8477ecb7e6517af690033226762dba0e404
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2020-12-04 15:18:53 +0300

    Style form

--- src/app/test-form/single-choice/single-choice.component.ts
+++ src/app/test-form/single-choice/single-choice.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { SingleChoice } from 'src/app/shared//models/single-choice.model';
 
 @Component({
